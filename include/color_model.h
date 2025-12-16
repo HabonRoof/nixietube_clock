758a3ba5a1ee5255b4fdab267e781bbc830c5395
JohnsonCL Chen <johnson35762@gmail.com>
2025-12-16 22:43:43 +0800

    feat(color): remove oklch color model and using hsv model for more convenient implementation

---
+++ include/color_model.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+
+struct RgbColor
+{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+};
+
+struct HsvColor
+{
+    uint16_t hue; // 0-360
+    uint8_t saturation; // 0-255
+    uint8_t value; // 0-255
+};
+
+RgbColor hsv_to_rgb(const HsvColor &hsv);
+RgbColor apply_gamma(const RgbColor &linear_color);
