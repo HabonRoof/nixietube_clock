d61d8d749915c5aae2e3d9f8acdde97874a790be
Johnson <johnson35762@gmail.com>
2026-08-10 20:42:33 +0800

    chore(pomodoro): update rest time to 5 min

--- src/daemons/display_daemon.h
+++ src/daemons/display_daemon.h
@@ -116,7 +116,7 @@ private:
     static constexpr uint8_t kCathodePoisonCtr = 15;
     static constexpr uint8_t kFadeStep = 51;
     static constexpr uint32_t kPomodoroWorkMs = 25 * 60 * 1000;
-    static constexpr uint32_t kPomodoroBreakMs = 1 * 60 * 1000;
+    static constexpr uint32_t kPomodoroBreakMs = 5 * 60 * 1000;
 
     INixieDriver &nixie_driver_;
     ILedDriver &led_driver_;
